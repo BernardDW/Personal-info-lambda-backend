3196d41e656eca2fbb794faf0414f5544b5e4d10
Berbaas <bdewet867@gmail.com>
2020-11-16 20:26:39 +0200

    Update user append list

--- userFunctions/updateUser.js
+++ userFunctions/updateUser.js
@@ -1,5 +1,6 @@
 var AWS = require("aws-sdk");
-var docClient = new AWS.DynamoDB.DocumentClient()
+var docClient = new AWS.DynamoDB.DocumentClient();
+var ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
 
 var table = "userPI";
 
@@ -8,39 +9,89 @@ exports.handler = async event => {
     var uuid = event.uuid;
     var metadata = event.metadata;
     metadata = metadata.replace("[", "");
-    console.log(metadata)
     metadata = metadata.replace("]", "");
-    console.log(metadata)
     metadata = metadata.split(",");
-    console.log(metadata)
     let _result = "";
-
-    var params = {
-        TableName:table,
-        Key:{
-            "uuid": uuid
-        },
-        UpdateExpression: "set metadata = list_append(metadata, :newdata)",
-        ExpressionAttributeValues: {
-            ":newdata": metadata
-        },
+    
+    var getParams = {
+      TableName: table,
+      Key: {
+        'uuid': {
+          S: uuid
+        }
+      },
+      ProjectionExpression: 'metadata'
     };
     
-    try {
-        console.log("Updating the item...");
-        _result = await docClient.update(params).promise();
-        const response = {
-          statusCode: 200,
-          headers: {
-            "Access-Control-Allow-Methods": "*",
-            "Access-Control-Allow-Origin" : "*" // Required for CORS
-          },
-        };
-        return response;
-    } catch (e) {
-        return {
-          statusCode: 500,
-          body: {metadata}
-        };
+    function isEmptyObject(obj) {
+      return !Object.keys(obj).length;
     }
+    
+    
+    console.log("Get the item...");
+    var result = await ddb.getItem(getParams).promise()
+    console.log(JSON.stringify(result));
+    var antw = result.Item;
+    
+    if (isEmptyObject(antw)){
+        var params1 = {
+            TableName:table,
+            Key:{
+                "uuid": uuid
+            },
+            UpdateExpression: "set metadata = :newdata",
+            ExpressionAttributeValues: {
+                ":newdata": metadata
+            },
+        };
+        
+        try {
+            console.log("Updating the item...");
+            _result = await docClient.update(params1).promise();
+            const response = {
+              statusCode: 200,
+              headers: {
+                "Access-Control-Allow-Methods": "*",
+                "Access-Control-Allow-Origin" : "*" // Required for CORS
+              },
+            };
+            return response;
+        } catch (e) {
+            return {
+              statusCode: 500,
+              body: {e}
+            };
+        }
+    }else{
+        var params = {
+            TableName:table,
+            Key:{
+                "uuid": uuid
+            },
+            UpdateExpression: "set metadata = list_append(metadata, :newdata)",
+            ExpressionAttributeValues: {
+                ":newdata": metadata
+            },
+        };
+        
+        try {
+            console.log("Updating the item...");
+            _result = await docClient.update(params).promise();
+            const response = {
+              statusCode: 200,
+              headers: {
+                "Access-Control-Allow-Methods": "*",
+                "Access-Control-Allow-Origin" : "*" // Required for CORS
+              },
+            };
+            return response;
+        } catch (e) {
+            return {
+              statusCode: 500,
+              body: {metadata}
+            };
+        }
+    }
+    
+    
 };
